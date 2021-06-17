b872ac61f76b028e6d4fae3ba3bfdd4c9e0188dd
IsaelSobral <isael.sobral@gmail.com>
2021-06-16 23:32:10 -0300

    teste

---
+++ variaveisGlobais.rb
@@ -0,0 +1,21 @@
+$cafePadrao = "Extra Forte" #variável global
+class Cafe
+  def initialize(nome)
+    @nome = nome
+  end
+  def tipo #método privado de café
+    puts $cafePadrao #acessando var global e imprimindo
+    $cafePadrao = "Cappuccino"
+    puts $cafePadrao
+  end
+end
+def tipo #método público
+  puts $cafePadrao = "Gratinado"
+end
+cafe = Cafe.new("Fraco"); cafe.tipo;
+puts $cafePadrao; tipo
+
+#criando constantes - é possível alterar valores de constantes, o Ruby alerta que alteração será feita
+Constante = 4
+Constante = $cafePadrao
+puts Constante
---
+++ variaveisInstancias.rb
@@ -0,0 +1,23 @@
+class Linguagem
+  @@Total=0 #variável de classe
+  def initialize(n) #construtor
+    @nome = n #variável de instância de classe
+    @@Total +=1 #sempre que um objeto Linguagem for criado será incrementado 1
+  end
+  def nome #método para chamar o valor colocado no parâmetro
+    puts @nome
+  end
+  def Linguagem.info #método de classe
+    puts @@Total
+  end
+end
+#instanciando objetos
+ruby = Linguagem.new("Ruby")
+php = Linguagem.new("PHP")
+java = Linguagem.new("Java")
+html = Linguagem.new("HTML")
+#imprimindo na tela
+puts "Minhas linguagens favoritas são:"
+puts ruby.nome; php.nome; java.nome; html.nome
+Linguagem.info
+
