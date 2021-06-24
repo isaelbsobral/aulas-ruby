bcca06e1e4dd3d9baf012b3a7698ee4785ac185a
IsaelSobral <isael.sobral@gmail.com>
2021-06-23 23:53:37 -0300

    Criando método de verificação de palídromo

--- Exercicios/Palindromo.rb
+++ Exercicios/Palindromo.rb
@@ -1,1 +1,13 @@
+#Verificar se uma palavra é uma palíndromo (se é igual de trás pra frente)
+#Utilize um método que vai receber o valor digitado pelo usuário
+# e verificar se é um palíndromo, imprimindo na tela o resultado
 
+def palindromo?(palavra)
+  #downcase -> transforma a palavra em minusculo
+  puts palavra.downcase == palavra.downcase.reverse ? "#{palavra} é um palíndromo": "#{palavra} não é um palíndromo"
+end
+
+puts "Informe a palavra para verificação"
+palavra_informada = gets.chomp
+
+palindromo?(palavra_informada)
