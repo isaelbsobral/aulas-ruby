23f0038b463f9d2995cd9695f9d0d83efc7bd1ab
IsaelSobral <isael.sobral@gmail.com>
2021-06-28 23:57:50 -0300

    Aprendendo conceitos de protected, privaste e public

--- Aulas/Visibilidade_de_metodos.rb
+++ Aulas/Visibilidade_de_metodos.rb
@@ -1,1 +1,25 @@
+class Gato
+  def andar
+    puts "Estou andando"
+    miar
+    ronronar
+  end
+  #Criando métodos privados - também podem ser acessados pelas classes filhas
+  # definindo visibilidade dos métodos:
+  #     private/public/   def nomeDoMetodo
+  #     end
+  private #Os métodos criados após essa linha só podem se acessados chamando o método público
+  #protected -> podem ser chamados por qualquer instância da mesma classe/superclasse
+  def miar
+    puts "Miau!"
+  end
+  def ronronar
+    puts "humrunrrurrum"
+  end
+end
+siames = Gato.new
+#siames.miar, siames.ronronar -> não podem ser acessados diretamentet, pois é privado
+#é necessário chamar o método público
+siames.andar
+
 
